a48bd97f8e710fe98cec619a64275f3fcb73d690
Ilya Margolin <marmon@hotmail.com>
2014-04-27 12:52:36 -0700

    Fix test

--- Mefisto.Fb2.UnitTests/AssociativeReaderTests.cs
+++ Mefisto.Fb2.UnitTests/AssociativeReaderTests.cs
@@ -11,11 +11,12 @@ using Xunit;
 
 namespace Mefisto.Fb2.UnitTests
 {
-	public class AssociativeReaderTests
+	public class AssociativeReaderTests : IDisposable
 	{
 		[NotNull] private readonly XmlReader _xmlReader;
 		[NotNull] private readonly Action<XmlReader> _a;
 		[NotNull] private readonly Action<XmlReader> _b;
+		private TestLogger _testLogger = new TestLogger();
 
 		public AssociativeReaderTests()
 		{
@@ -33,10 +34,15 @@ namespace Mefisto.Fb2.UnitTests
 			A.CallTo(() => _b(_xmlReader)).Invokes(() => _xmlReader.Name.Should().Be("B"));
 		}
 
+		public void Dispose()
+		{
+			_testLogger.Messages.Should().Equal(new string[0]);
+		}
+
 		[Fact]
 		public void Scan_Should_Fire_Associated_Callbacks()
 		{
-			var reader = new AssociativeReader(new TestLogger(), _xmlReader,
+			var reader = new AssociativeReader(_testLogger, _xmlReader,
 				new ScopeHandler("R",
 					new ScopeHandler("A", _a),
 					new ScopeHandler("B", _b)));
@@ -46,16 +52,35 @@ namespace Mefisto.Fb2.UnitTests
 			A.CallTo(() => _a(_xmlReader)).MustHaveHappened();
 			A.CallTo(() => _b(_xmlReader)).MustHaveHappened();
 		}
+
+		[Fact]
+		public void Lack_Of_Mandatory_Element_Should_Log_Errors()
+		{
+			var reader = new AssociativeReader(_testLogger, _xmlReader,
+				new ScopeHandler("R",
+					new ScopeHandler("D")
+					{
+						Mandatory = true,
+					}));
+
+			reader.Scan();
+
+			_testLogger.DequeueMessages().Should().Equal("x");
+		}
 	}
 
-	[DebuggerDisplay("{_name}: {SubDescriptors.Count}")]
+	[DebuggerDisplay("{Name}: {SubDescriptors.Count}")]
 	public class ScopeHandler
 	{
-		[NotNull] private readonly XName _name;
+		[NotNull] public XName Name { get; private set; }
 
-		[CanBeNull] public Action<XmlReader> Handler { get; private set; }
+		[CanBeNull]
+		public Action<XmlReader> Handler { get; private set; }
 
-		[NotNull] public Dictionary<XName, ScopeHandler> SubDescriptors { get; private set; }
+		[NotNull]
+		public Dictionary<XName, ScopeHandler> SubDescriptors { get; private set; }
+
+		public bool Mandatory { get; set; }
 
 		public ScopeHandler([NotNull] XName name, [NotNull] Action<XmlReader> handler)
 			: this(name)
@@ -65,33 +90,39 @@ namespace Mefisto.Fb2.UnitTests
 
 		public ScopeHandler([NotNull] XName name, [NotNull] params ScopeHandler[] subScopeHandlers)
 		{
-			_name = name;
-			SubDescriptors = subScopeHandlers.ToDictionary(x => x._name, x => x);
+			Name = name;
+			SubDescriptors = subScopeHandlers.ToDictionary(x => x.Name, x => x);
 		}
 	}
 
 	public class Scope
 	{
-		public ScopeHandler Handler { get; set; }
-		public Scope ParentScope { get; set; }
+		[NotNull]
+		public ScopeHandler Handler { get; private set; }
 
-		public Scope(ScopeHandler handler )
+		[NotNull]
+		public Scope ParentScope { get; private set; }
+
+		public Scope([NotNull] ScopeHandler handler)
 		{
 			Handler = handler;
 		}
 
-		public Scope(Scope parentScope, ScopeHandler handler)
+		public Scope([NotNull] Scope parentScope, [NotNull] ScopeHandler handler)
 			: this(handler)
 		{
 			ParentScope = parentScope;
 		}
 	}
+
 	public class AssociativeReader
 	{
 		private readonly ILogger _testLogger;
 		private readonly XmlReader _reader;
+		private readonly ScopeHandler _rootScopeHandler;
 		private ScopeHandler _scopeHandler;
 		private Scope _currentScope;
+
 		public AssociativeReader(
 			[NotNull] ILogger testLogger,
 			[NotNull] XmlReader reader,
@@ -99,25 +130,47 @@ namespace Mefisto.Fb2.UnitTests
 		{
 			_testLogger = testLogger;
 			_reader = reader;
-			_scopeHandler = scopeHandler;
-			_currentScope = new Scope(scopeHandler);
+			_rootScopeHandler = scopeHandler;
 		}
 
-
+		private int _virtualScopeCounter;
 		public void Scan()
 		{
 			while (_reader.Read())
 			{
 				if (_reader.NodeType == XmlNodeType.EndElement)
 				{
+					if (_virtualScopeCounter > 0)
+					{
+						_virtualScopeCounter --;
+						continue;
+					}
 					_scopeHandler = _currentScope.Handler;
 					_currentScope = _currentScope.ParentScope;
 					continue;
 				}
+				if (_virtualScopeCounter > 0)
+				{
+					if (!_reader.IsEmptyElement)
+						_virtualScopeCounter++;
+					continue;
+				}
 				XNamespace nsp = _reader.NamespaceURI;
 				var name = nsp + _reader.Name;
+				if (_scopeHandler == null)
+				{
+					Debug.Assert(name == _rootScopeHandler.Name);
+					_scopeHandler = _rootScopeHandler;
+					_currentScope = new Scope(_scopeHandler);
+					continue;
+				}
 				ScopeHandler scopeHandler;
-				if (!_scopeHandler.SubDescriptors.TryGetValue(name, out scopeHandler)) continue;
+				if (!_scopeHandler.SubDescriptors.TryGetValue(name, out scopeHandler))
+				{
+					if (!_reader.IsEmptyElement)
+						_virtualScopeCounter++;
+					continue;
+				}
 				if (!_reader.IsEmptyElement)
 				{
 					_currentScope = new Scope(_currentScope, _scopeHandler);
