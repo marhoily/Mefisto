b4fea22824ee364d188084bbc78d00a109469343
Ilya Margolin <marmon@hotmail.com>
2014-04-27 10:03:31 -0700

    Add BookReader

---
+++ Mefisto.Fb2.UnitTests/BookReaderTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Mefisto.Fb2.UnitTests
+{
+	public class BookReaderTests
+	{
+		[Fact]
+		public void Read_When_Correct_Tag_Should_Return_Book()
+		{
+			var bookReader = new BookReader();
+			var book = bookReader.Read(
+				new XElement(Xmlns.Fb2 + "FictionBook").CreateReader());
+			book.Should().NotBeNull();
+		}
+		[Fact]
+		public void Read_When_Incorrect_Namespace_Should_Return_Null()
+		{
+			var bookReader = new BookReader();
+			var book = bookReader.Read(
+				new XElement("FictionBook").CreateReader());
+			book.Should().BeNull();
+		}
+		[Fact]
+		public void Read_When_Incorrect_Tag_Should_Return_Null()
+		{
+			var bookReader = new BookReader();
+			var book = bookReader.Read(
+				new XElement(Xmlns.Fb2 + "wrong").CreateReader());
+			book.Should().BeNull();
+		}
+	}
+}
---
+++ Mefisto.Fb2/BookReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml;
+
+namespace Mefisto.Fb2
+{
+	public class BookReader
+	{
+		private const string Fb2 = "http://www.gribuser.ru/xml/fictionbook/2.0";
+
+		public Book Read(XmlReader reader)
+		{
+			reader.Read();
+			if (reader.Name != "FictionBook")
+				return null;
+			if (reader.NamespaceURI != Fb2)
+				return null;
+			return new Book();
+		}
+	}
+
+	public class Book
+	{
+	}
+}
